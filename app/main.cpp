43a44ec0bceade2336be0fb44c0260cad57cdba9
K1MHJ <hojokimu@gmail.com>
2022-08-28 14:13:01 +0900

    get error description

--- app/main.cpp
+++ app/main.cpp
@@ -1,16 +1,15 @@
-//#include "../include/shape.h"
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <iostream>
+#include <errno.h>
 
 int main() 
 {
     int sock;
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(3000,4000,5000);
     if(sock < 0){
-        std::cout << "socket failed" <<std::endl;
+        perror("socket");
+        printf("%d\n",errno);
         return 1;
     }
     return 0;
