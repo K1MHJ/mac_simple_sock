9cba1ef568ff146790aaa86045acea9f677778ff
K1MHJ <hojokimu@gmail.com>
2022-08-28 14:10:29 +0900

    create socket

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,17 @@
-#include "../include/shape.h"
+//#include "../include/shape.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <iostream>
 
-int main() {
-    std::cout << test::add(10,2) << std::endl;
-  return 0;
+int main() 
+{
+    int sock;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0){
+        std::cout << "socket failed" <<std::endl;
+        return 1;
+    }
+    return 0;
 }
